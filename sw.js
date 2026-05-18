37abb2a3ffcc77ae904f9531854df34128bdc581
T0bib0y <tobyboy129@gmail.com>
2026-05-18 13:32:06 +0200

    Add service worker sw.js for PWA offline support

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'notebook-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+  // Note: CDN resources (Tailwind, Marked, Font Awesome) will be cached by browser after first load
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
